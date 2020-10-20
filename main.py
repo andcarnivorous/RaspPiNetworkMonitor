c9ba7f3e5588932d766a3bb945a5d084e851256b
andcarnivorous <drewlinguistics01@gmail.com>
2020-10-20 20:44:25 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import lcddriver
+import time
+import utils
+import os
+
+display = lcddriver.lcd()
+
+if "database.db" not in os.listdir():
+    
+    newtable = """ CREATE TABLE IF NOT EXISTS visitors (
+    ip TEXT,
+    name TEXT,
+    date TEXT
+    ); """
+
+    with utils.create_connection("database.db") as conn:
+        utils.create_table(conn, newtable)
+
+try:
+    while True:
+        # Remember that your sentences can only be 16 characters long!
+        ipadd = ".".join(utils.get_ip_address())
+        ips, names = utils.give_ips()
+        display.lcd_clear()
+
+        for ip in range(1,len(ips)):
+            display.lcd_display_string("# Devices: %d" % len(ips), 1) # Write line of text to first line of display
+            display.lcd_display_string("# %d: %s" % (ip, ips[ip]), 3) # Write line of text to first line of display
+            display.lcd_display_string("Name: %s" % names[ip][:13], 4)
+            time.sleep(2)
+            display.lcd_clear()
+
+        display.lcd_display_string("Scanning network...", 1)
+        display.lcd_display_string("MyIP : %s" % ipadd, 4) # Write line of text to second line of display
+
+except KeyboardInterrupt: # If there is a KeyboardInterrupt (when you press ctrl+c), exit the program and cleanup
+    print("Cleaning up!")
+    display.lcd_clear()
---
+++ utils.py
@@ -0,0 +1,88 @@
+import datetime
+import socket
+import subprocess
+import re
+import sqlite3
+from functools import wraps
+from sqlite3 import Error
+
+
+
+        
+
+
+def create_connection(db_file):
+    """ create a database connection to a SQLite database """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            return conn
+
+
+
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+
+    except Error as e:
+        print(e)
+
+def send_to_db(func):
+    @wraps(func)
+    def wrapper():
+        ips, names = func()
+        today = datetime.datetime.now().strftime("%m-%d-%Y %H:%M:%S")
+        for ip, name in zip(ips, names):
+            new_entry(ip, name, today)
+        return (ips, names)
+    return wrapper
+
+def new_entry(ip: str, name: str, date: str):
+    """
+    """
+    sql = """INSERT INTO visitors (ip, name, date)
+    VALUES ('%s', '%s', '%s')""" % (ip, name, date)
+    with create_connection("database.db") as conn:
+        c = conn.cursor()
+        try:
+            c.execute(sql)
+        except sqlite3.Error as error:
+            return Error
+        conn.commit()
+
+
+        
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0].split(".")
+
+
+def scan_network():
+
+    ip = get_ip_address()
+    ip = ".".join(ip[:-1])
+
+    result = subprocess.run(['nmap', '-sn', f"{ip}.0/24"], stdout=subprocess.PIPE)
+    return str(result.stdout.decode("utf-8"))
+
+
+@send_to_db
+def give_ips():
+
+    regex = "\((\d+\.\d+.\d+.\d+)\)"
+    scan = scan_network()
+    regex2 = "for ([\.\-\_\w\d]+) \("
+    ips = re.findall(regex, scan)
+    names = re.findall(regex2,scan)
+    return (ips, names)
